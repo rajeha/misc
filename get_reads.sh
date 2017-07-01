5b6e507aaf66801b45dfcd5074ca44d6c784088c
rajeha <ahmadrjeh@gmail.com>
2017-06-30 21:01:49 -0500

    fix perl conflict with shell variables

--- get_reads.sh
+++ get_reads.sh
@@ -14,8 +14,8 @@ rh=${4:-@SR} # default read header starts with @SR
 
 samtools view -h $1 $2 | samtools fastq -1 r1.tmp -2 r2.tmp -
 
-perl -pi -e "s/\$rh.+\S/\$&\/1/" r1.tmp 
-perl -pi -e "s/\$rh.+\S/\$&\/2/" r2.tmp 
+perl -spi -e 's/$rh.+\S/$&\/1/' -- -rh="$rh" r1.tmp 
+perl -spi -e 's/$rh.+\S/$&\/2/' -- -rh="$rh" r2.tmp 
 
 fix_pairs.pl r1.tmp r2.tmp $opref
 
